a666cc965d6f487ec283a26a7ddf31f0ddb87a47
JcFace <jcv@gmail.com>
2020-10-07 19:10:21 -0700

    Jesse/Inee Code Breaker Team

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+
+
+class School
+
+    attr_accessor :grade
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    #    @roster[grade] = []
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else 
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade) 
+         @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, kids|
+           kids.sort!
+            #  binding.pry  
+        end
+    end
+end
